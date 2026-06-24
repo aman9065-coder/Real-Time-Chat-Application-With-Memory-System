cce545a3a4b6b46bc5c2cf165a3c74183d3ec7e8
Aman kumar <tony2k219065@gammil.com>
2026-06-24 14:32:47 +0530

    Update production API URL

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,13 +1,26 @@
+// import axios from "axios";
+
+// const api = axios.create({
+//   baseURL: "http://localhost:3000/api", // backend url
+//   withCredentials: true // cookies / jwt
+// });
+
+// // const api = axios.create({
+// //   baseURL: import.meta.env.VITE_API_URL,
+// //   withCredentials: true,
+// // });
+
+// export default api;
+
+
+// deploy
+
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:3000/api", // backend url
-  withCredentials: true // cookies / jwt
+  // backend ka live url rebder pr
+  baseURL: "https://real-time-chat-application-with-memory-bt70.onrender.com/api",
+  withCredentials: true,
 });
-
-// const api = axios.create({
-//   baseURL: import.meta.env.VITE_API_URL,
-//   withCredentials: true,
-// });
 
 export default api;
